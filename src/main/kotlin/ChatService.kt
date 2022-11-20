feb78f3168bc4490e593ec850bce23b5662acff9
Vitalii Novgorodov <fivcheg@gmail.com>
2022-11-20 13:53:09 +0300

    Add sequences

--- src/main/kotlin/ChatService.kt
+++ src/main/kotlin/ChatService.kt
@@ -5,32 +5,58 @@ object ChatService {
     fun clear() {
         chats = mutableMapOf()
     }
-    fun addMessage(recepient: Int, message: Message) {
+
+    fun addMessage(recepient: Int, message: Message) =
         chats.getOrPut(recepient) { Chat() }.messages.add(message)
-    }
+
 
     fun printChats() {
         println(chats)
     }
 
-    fun unreadChatsCount() = chats.values.count { chat -> chat.messages.any { !it.read } }
+    fun unreadChatsCount() = chats.values.asSequence().count { chat -> chat.messages.any { !it.read } }
 
-    fun getMessages(recepient: Int, messagesCount: Int): List<Message> {
-        return chats[recepient]?.messages.orEmpty().takeLast(messagesCount).onEach { it.read = true }
-    }
+    fun getMessages(recepient: Int, messagesCount: Int): List<Message> =
+        chats
+            .filter { it.key == recepient }
+            .values
+            .asSequence()
+            .map { it.messages }
+            .take(messagesCount)
+            .ifEmpty { throw ChatNotFoundError() }
+            .last()
+            .onEach { it.read = true }
 
     fun deleteMessages(recepient: Int, messageId: Int) {
-        chats[recepient]?.messages.orEmpty().take(messageId).onEach { it.delete = true }
-        if (chats[recepient]?.messages.orEmpty().count { !it.delete } == 0) {
-            chats.remove(recepient)
-        }
+        chats
+            .filter { it.key == recepient }
+            .values
+            .asSequence()
+            .map { it.messages }
+            .take(messageId)
+            .ifEmpty { throw MessageNotFoundError() }
+            .last()
+            .onEach { it.delete = true }
+            .count { !it.delete }
+            .takeIf { it == 0 }
+            ?: run {
+                chats.remove(recepient)
+            }
     }
 
     fun deleteChat(recepient: Int) {
         chats.remove(recepient)
     }
 
-    fun getChats() = chats.mapValues { it.value.messages[0] }
+    fun getChats() = chats
+        .values
+        .asSequence()
+        .map { it.messages[0] }
+        //.forEach(::println)
+
+    class ChatNotFoundError : Exception("Чат не найден!")
+    class MessageNotFoundError : Exception("Сообщение не найдено!")
+
 }
 
 
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,6 +6,7 @@ fun main() {
     ChatService.addMessage(3, Message("aaasd", false, false))
     ChatService.addMessage(3, Message("2342aaasd", false, false))
     ChatService.addMessage(4, Message("aaasd", true, false))
-    ChatService.printChats()
+  //  ChatService.printChats()
     println(ChatService.getChats())
+
 }
